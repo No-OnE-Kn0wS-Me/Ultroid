ad898fdf33a76ddfcebca9a90c3ddc3ca0f2ec33
JyothisJayanth <65120517+JyothisJayanth@users.noreply.github.com>
2020-06-09 16:11:00 +0530

    Add files via upload

---
+++ userbot/plugins/GetLink.py
@@ -0,0 +1,38 @@
+import datetime
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
+from uniborg.util import admin_cmd
+
+@borg.on(admin_cmd(pattern="getlink ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return 
+    if not event.reply_to_msg_id:
+       await event.edit("```Reply To Any File!```")
+       return
+    reply_message = await event.get_reply_message() 
+    if not reply_message.text:
+       await event.edit("```Reply To A File!```")
+       return
+    chat = "@highspeedlinksbot"
+    sender = reply_message.sender
+    if reply_message.sender.bot:
+       await event.edit("```Reply To Any Link!```")
+       return
+    await event.edit("```Generating Link.. Please wait!!```")
+    async with event.client.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1071746481))
+              await event.client.forward_messages(chat, reply_message)
+              response = await response 
+          except YouBlockedUserError: 
+              await event.reply("```Please unblock Files to LinkBot```")
+              return
+          if response.text.startswith("Send a telegram file to get its directlink."):
+             await event.edit("```Disable Your Forward Privacy Settings!```")
+          else: 
+             response.text.startswith("Direct Link generated"):
+             return
+             await event.delete()
+             await event.client.send_message(event.chat_id, response.message)
